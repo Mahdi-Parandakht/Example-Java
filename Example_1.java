04b39ba345b44a09491aa76c17687fe11763c39c
Mohammad Mehdi Parandokht <161738485+Mahdi-Parandakht@users.noreply.github.com>
2025-02-23 08:19:50 -0800

    Add files via upload

---
+++ Example_1.java
@@ -0,0 +1,8 @@
+public class Example_1{
+    public static void main(String[] args) {
+        String fname = "Mehdi ";
+        String lname = "Parandokht";
+        String full_name = fname + lname;
+        System.out.println("Hi " + full_name);
+    }
+}
---
+++ Example_2.java
@@ -0,0 +1,12 @@
+public class Example_2 {
+    public static void main(String[] args) {
+        int length = 7;
+        int width = 4;
+        int Perimeter = 2 * (length + width);
+        int Area = length * width;
+        System.out.println("length: " + length);
+        System.out.println("width: " + width);
+        System.out.println("Perimeter: " + Perimeter);
+        System.out.println("Area: " + Area);
+    }
+}
